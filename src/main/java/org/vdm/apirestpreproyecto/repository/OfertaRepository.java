deaa2618d911171409e0e7296803f5c492441958
Markex2002 <mmarber462@g.educaand.es>
2024-06-15 00:37:02 +0200

    Actualizacion Proyecto

--- src/main/java/org/vdm/apirestpreproyecto/repository/OfertaRepository.java
+++ src/main/java/org/vdm/apirestpreproyecto/repository/OfertaRepository.java
@@ -3,9 +3,20 @@ package org.vdm.apirestpreproyecto.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.vdm.apirestpreproyecto.domain.Administrador;
 import org.vdm.apirestpreproyecto.domain.OfertaTrabajo;
+
+import java.util.List;
 
 @Repository
 public interface OfertaRepository extends JpaRepository<OfertaTrabajo, Long> {
+    //BUSCAMOS POR SALARIO BRUTO MINIMO
+    public List<OfertaTrabajo> findBySalarioBrutoMinGreaterThan(int cantidadMin);
+    public List<OfertaTrabajo> findBySalarioBrutoMinGreaterThanOrderBySalarioBrutoMinAsc(int nombre);
+    public List<OfertaTrabajo> findBySalarioBrutoMinGreaterThanOrderBySalarioBrutoMinDesc(int nombre);
 
+
+    //ORDENAMOS POR FECHA PUBLICACION
+    public List<OfertaTrabajo> findAllByOrderByFechaPublicacionAsc();
+    public List<OfertaTrabajo> findAllByOrderByFechaPublicacionDesc();
 }
--- src/test/java/org/vdm/apirestpreproyecto/ApiRestPreProyectoApplicationTests.java
+++ src/test/java/org/vdm/apirestpreproyecto/ApiRestPreProyectoApplicationTests.java
@@ -4,10 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.vdm.apirestpreproyecto.domain.*;
-import org.vdm.apirestpreproyecto.repository.AdministradorRepository;
-import org.vdm.apirestpreproyecto.repository.ArtistaRepository;
-import org.vdm.apirestpreproyecto.repository.EmpresaRepository;
-import org.vdm.apirestpreproyecto.repository.ImagenRepository;
+import org.vdm.apirestpreproyecto.repository.*;
 
 import java.math.BigDecimal;
 import java.util.*;
@@ -23,6 +20,8 @@ class ApiRestPreProyectoApplicationTests {
     ImagenRepository imagenRepository;
     @Autowired
     AdministradorRepository administradorRepository;
+    @Autowired
+    OfertaRepository ofertaRepository;
 
     @Test
     void contextLoads() {}
@@ -34,29 +33,35 @@ class ApiRestPreProyectoApplicationTests {
 
     @Test
     void pruebaCRUDTodo(){
-        //PRUEBA CREAR Y GUARDAR UN ARTISTA
+        //PRUEBA CREAR Y GUARDAR VARIOS ARTISTA
         Artista artista1 = Artista.builder()
                 .nombre("VanGogh")
                 .build();
         artistaRepository.save(artista1);
-        //PRUEBA CREAR Y GUARDAR UN ARTISTA
         Artista artista2 = Artista.builder()
                 .nombre("SrPelo")
                 .build();
         artistaRepository.save(artista2);
 
         //Prueba crear y guardar varias Empresa
-        Empresa empresa = new Empresa();
-        empresa.setNombreEmpresa("Nintendo");
-        empresaRepository.save(empresa);
-
-        Empresa empresa1 = new Empresa();
-        empresa1.setNombreEmpresa("Sega");
+        Empresa empresa1 = Empresa.builder()
+                .nombreEmpresa("Nintendo")
+                .numTlf(521522523)
+                .build();
         empresaRepository.save(empresa1);
 
-        Empresa empresa2 = new Empresa();
-        empresa2.setNombreEmpresa("Capcom");
+        Empresa empresa2 = Empresa.builder()
+                .nombreEmpresa("Sega")
+                .numTlf(123456789)
+                .build();
         empresaRepository.save(empresa2);
+
+        Empresa empresa3 = Empresa.builder()
+                .nombreEmpresa("Capcom")
+                .numTlf(222333444)
+                .build();
+        empresaRepository.save(empresa3);
+
 
         //Prueba crear y guardar varios Administradores
         Administrador administrador1 = Administrador.builder()
@@ -79,7 +84,6 @@ class ApiRestPreProyectoApplicationTests {
                 .nombre("Luigi")
                 .build();
         administradorRepository.save(administrador4);
-
 
 
 
@@ -109,24 +113,34 @@ class ApiRestPreProyectoApplicationTests {
         imagenRepository.save(imagen3);
         imagenRepository.save(imagen4);
 
-        //Creamos un PortFolio y lo insertamos en el Artista
+        //Creamos un PortFolio y lo insertamos en el Artista//
         List<Imagen> listaImagenes1 = new ArrayList<>();
         listaImagenes1.add(imagen1);
         listaImagenes1.add(imagen2);
         artista1.setPortfolio(listaImagenes1);
 
-        //Creamos una lista con el Artista
+        //Creamos una lista con el Artista//
         Set<Artista> listaArtistas = new HashSet<>();
         listaArtistas.add(artista1);
 
 
-        OfertaTrabajo ofertaTrabajo = OfertaTrabajo.builder()
+        //CREAMOS VARIAS OFERTAS DE TRABAJO//
+        OfertaTrabajo ofertaTrabajo1 = OfertaTrabajo.builder()
+                .empresa(empresa1)
                 .duracionJornada(20)
                 .avaiablePositions(2)
                 .salarioBrutoMin(10000)
                 .salarioBrutoMax(14000)
                 .artistas(listaArtistas)
                 .build();
-
+        ofertaRepository.save(ofertaTrabajo1);
+        OfertaTrabajo ofertaTrabajo2 = OfertaTrabajo.builder()
+                .empresa(empresa2)
+                .duracionJornada(20)
+                .avaiablePositions(2)
+                .salarioBrutoMin(6000)
+                .salarioBrutoMax(20000)
+                .build();
+        ofertaRepository.save(ofertaTrabajo2);
     }
 }
